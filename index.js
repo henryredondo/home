1b485505bd3a121d6ec8a75967da6451a7df8b69
cvarelaf <36269149+cvarelaf@users.noreply.github.com>
2021-05-31 16:57:06 -0600

    Footer Fix

---
+++ index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+  });
+
+  sr.reveal('.header-content-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+  });
+
+  sr.reveal('.header-content-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
